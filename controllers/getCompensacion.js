a72a2df8ac3a93d1cdb50bd5ae4571e20e07be91
Yerko <yerkoortega14@gmail.com>
2020-11-03 14:10:41 -0300

    obtener info compensacion

--- controllers/getCompensacion.js
+++ controllers/getCompensacion.js
@@ -11,7 +11,13 @@ const getCompensacion = async () => {
                                     ON(PERS.ID_PERSONA = MED.PERSONAS_ID_PERSONA)
                                     JOIN EXAMENES EX
                                     ON(EX.ID_EXAMEN=MED.EXAMENES_ID_EXAMEN)
-                                    WHERE PERS.RUT = :rutbv`;
+                                    WHERE PERS.ROLES_ID_ROL = :id_rolbv`;
+
+    const QUERY_GET_COMPENSACION2 = `SELECT PERS.RUT, PAR.PA_SIST, PAR. PA_DIAST 
+                                    FROM PARAMETROS PAR
+                                    JOIN PERSONAS PERS
+                                    ON (PERS.ID_PERSONA = PAR.PERSONAS_ID_PERSONA)
+                                    WHERE PERS.ROLES_ID_ROL = :id_rolbv`;
 
 
     //bv significa el valor que espera como parametro. En este caso id.
@@ -24,21 +30,49 @@ const getCompensacion = async () => {
 
         const result = await connection.execute(
             QUERY_GET_COMPENSACION,
-            [199745588],
+            [1],
             {
                 maxRows: 0
-            });
+            }
+        );
+
+        const result2 = await connection.execute(
+            QUERY_GET_COMPENSACION2,
+            [1],
+            {
+                maxRows: 0
+            }
+        );
 
         const data = result.rows.map(row => {
 
             const obj = new Object();
             obj.rut = row[0];
-            obj.nombre_examen = row[1];
-            obj.valor_examen = row[2];
+            let nombre = row[1];
+            obj[nombre] = row[2];
             return obj;
         })
 
-        const js = JSON.stringify(data);
+        const data2 = result2.rows.map(row => {
+
+            const obj = new Object();
+            obj.rut = row[0];
+            obj.pa_sis = row[1];
+            return obj;
+        })
+
+        const data3 = result2.rows.map(row => {
+
+            const obj = new Object();
+            obj.rut = row[0];
+            obj.pa_diast = row[2];
+            return obj;
+        })
+
+        const array3 = data.concat(data2);
+        const array4 = data2.concat(array3);
+
+        const js = JSON.stringify(array4);
         return js;
 
     } catch (err) {
--- index.js
+++ index.js
@@ -3,11 +3,9 @@ require('dotenv').config();
 const cors = require('cors');
 const getInfoPaciente = require ("./controllers/getInfoPaciente");
 const getUsuario = require ("./controllers/getUsuario");
-
+const getCompensacion = require ("./controllers/getCompensacion");
 const getEnfermedadPaciente = require ("./controllers/getEnfermedadesPaciente");
 const getAgenda = require('./controllers/getAgenda');
-const { response } = require('express');
-const getCompensacion = require('./controllers/getCompensacion');
 const port = 4000;
 
 const app = express();
@@ -27,26 +25,22 @@ app.get('/getAgenda', (req,res) => {
 })
 
 app.get('/getUsuario', (req,res) => {
-
-    
     getUsuario.then((response) => {
         res.send(response);
     });
 })
 
-app.get('/getCompensacion',(req,res)=>{
-    getCompensacion.then((response)=>{
-        res.send(response)
-    })
+app.get('/getCompensacion', (req,res) => {
+    getCompensacion.then((response) => {
+        res.send(response);
+    });
 })
-
 
 app.get('/getEnfermedadPaciente', (req,res) => {
     getEnfermedadPaciente.then((response) => {
         res.send(response);
     });
 })
-
 
 app.listen(port, () => {
     console.log(`server puerto ${port}`);
