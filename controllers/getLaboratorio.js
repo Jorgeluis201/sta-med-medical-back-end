99bf788ce56164748a5895b9c2a4843481b6b30b
Yerko <yerkoortega14@gmail.com>
2020-11-05 16:40:39 -0300

    getLaboratorio2

--- controllers/getLaboratorio.js
+++ controllers/getLaboratorio.js
@@ -80,30 +80,6 @@ const getLaboratorio = async () => {
             }
         );
 
-        const result8 = await connection.execute(
-            QUERY_GET_USUARIO,
-            [8],
-            {
-                maxRows: 0
-            }
-        );
-
-        const result9 = await connection.execute(
-            QUERY_GET_USUARIO,
-            [9],
-            {
-                maxRows: 0
-            }
-        );
-
-        const result10 = await connection.execute(
-            QUERY_GET_USUARIO,
-            [10],
-            {
-                maxRows: 0
-            }
-        );
-
         const data = result.rows.map(row => {
 
             const obj = new Object();
@@ -216,65 +192,14 @@ const getLaboratorio = async () => {
             return obj;
         })
 
-        const data8 = result8.rows.map(row => {
-
-            const obj = new Object();
-            obj.condicion_cr = row[0];
-            obj.sexo_masculino = row[1];
-            obj.sexo_femenino = row[2];
-            obj.nivel_prioridad = row[3];
-            obj.nombre_examen = row[4];
-            obj.corte_sub = row[5];
-            obj.corte_normal = row[6];
-            obj.corte_E1 = row[7];
-            obj.corte_E2 = row[8];
-            obj.direccion = row[9];
-            return obj;
-        })
-
-        const data9 = result9.rows.map(row => {
-
-            const obj = new Object();
-            obj.condicion_cr = row[0];
-            obj.sexo_masculino = row[1];
-            obj.sexo_femenino = row[2];
-            obj.nivel_prioridad = row[3];
-            obj.nombre_examen = row[4];
-            obj.corte_sub = row[5];
-            obj.corte_normal = row[6];
-            obj.corte_E1 = row[7];
-            obj.corte_E2 = row[8];
-            obj.direccion = row[9];
-            return obj;
-        })
-
-        const data10 = result10.rows.map(row => {
-
-            const obj = new Object();
-            obj.condicion_cr = row[0];
-            obj.sexo_masculino = row[1];
-            obj.sexo_femenino = row[2];
-            obj.nivel_prioridad = row[3];
-            obj.nombre_examen = row[4];
-            obj.corte_sub = row[5];
-            obj.corte_normal = row[6];
-            obj.corte_E1 = row[7];
-            obj.corte_E2 = row[8];
-            obj.direccion = row[9];
-            return obj;
-        })
-
         const array3 = data.concat(data2);
         const array4 = array3.concat(data3);
         const array5 = array4.concat(data4);
         const array6 = array5.concat(data5);
         const array7 = array6.concat(data6);
         const array8 = array7.concat(data7);
-        const array9 = array8.concat(data8);
-        const array10 = array9.concat(data9);
-        const array11 = array10.concat(data10);
 
-        const js = JSON.stringify(array11);
+        const js = JSON.stringify(array8);
         return js;
 
     } catch (err) {
