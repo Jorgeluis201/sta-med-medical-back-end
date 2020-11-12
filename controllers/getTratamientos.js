a9ebe8e17a75b3bddaf86434d70d7a4e8e16ecab
Yerko <yerkoortega14@gmail.com>
2020-11-12 20:31:07 -0300

    getTratamientos2

--- controllers/getTratamientos.js
+++ controllers/getTratamientos.js
@@ -2,7 +2,8 @@ const getInfoPaciente = async () => {
 
     const oracledb = require('oracledb');
     const dbConfig = require('../database/dbconfig');
-    const QUERY_GET_USUARIO = `SELECT pers.rut ,count(pers.id_persona), med.nombre_breve ||': '||trunc(rec.dosis_indic/30, 2) ||' c/'|| rec.fraccion_indic||' hrs' as "dosis_diaria", cr.abreviatura
+    const QUERY_GET_USUARIO = `SELECT pers.rut ,count(pers.id_persona), med.nombre_breve ||': '||trunc(rec.dosis_indic/30, 2) ||' c/'|| rec.fraccion_indic||' hrs' as "dosis_diaria", 
+                                cr.abreviatura, info.respuesta
                                 FROM medicamentos med
                                 JOIN recetados rec 
                                 ON(med.id_medicamento = rec.medicamentos_id_medicamento)
@@ -14,8 +15,10 @@ const getInfoPaciente = async () => {
                                 ON(us.enfermedades_id_enfermedad = enf.id_enfermedad)
                                 JOIN condicion_cr cr 
                                 ON(cr.id_condicion = enf.condicion_cr_id_condicion)
+                                JOIN informe_consumos info
+                                ON (info.id_consumo = rec.informe_consumos_id_consumo)
                                 WHERE ROLES_ID_ROL = :roles_id_rolbv
-                                group by pers.rut, med.nombre_breve, rec.dosis_indic, rec.fraccion_indic, cr.abreviatura`;
+                                group by pers.rut, med.nombre_breve, rec.dosis_indic, rec.fraccion_indic, cr.abreviatura, info.respuesta`;
 
     //bv significa el valor que espera como parametro. En este caso id.
 
@@ -38,6 +41,7 @@ const getInfoPaciente = async () => {
             obj.rut = row[0];
             obj.dosis_diaria = row[2];
             obj.con_cronica = row[3];
+            obj.consumo_medicamento = row[4];
             return obj;
         })
 
